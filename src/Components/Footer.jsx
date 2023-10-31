af9bf523b94aacd1aa9594550b54ac4681d38468
Oubaid <oubaidm@live.com>
2023-10-31 18:23:08 +0100

    finished footer mobile and started on footer desktop, still have instagram feed to implement

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,45 +8,60 @@ import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 function FooterNav({ items }) {
     let itemsList = items.map((item, i) => {
-        return <li key={i}>{item}</li>;
+        return (
+            <li key={i}>
+                <a href="#">{item}</a>
+            </li>
+        );
     });
 
     return (
-        <ul>
-            <itemList />
-        </ul>
+        <div className="footer_nav_container flex column">
+            <h2>Navigation</h2>
+            <nav className="footer_nav">
+                <ul className="flex column">{itemsList}</ul>
+            </nav>
+        </div>
     );
 }
 function Socials() {
     return (
-        <div className="socials">
-            <FontAwesomeIcon icon={faInstagram} />
-            <FontAwesomeIcon icon={faDribbble} />
-            <FontAwesomeIcon icon={faXTwitter} />
-            <FontAwesomeIcon icon={faYoutube} />
+        <div className="socials flex justify_center">
+            <a href="">
+                <FontAwesomeIcon icon={faInstagram} className="socials_icon" />
+            </a>
+            <a href="">
+                <FontAwesomeIcon icon={faDribbble} className="socials_icon" />
+            </a>
+            <a href="">
+                <FontAwesomeIcon icon={faXTwitter} className="socials_icon" />
+            </a>
+            <a href="">
+                <FontAwesomeIcon icon={faYoutube} className="socials_icon" />
+            </a>
         </div>
     );
 }
 
 function Footer() {
     return (
-        <footer className="footer grid">
+        <footer className="footer grid justify_center">
             <Logo />
-            <nav>
-                <ul>
-                    <li>About</li>
-                    <li>Features</li>
-                    <li>Pricing</li>
-                    <li>Careers</li>
-                    <li>Help</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </nav>
+            <FooterNav
+                items={[
+                    "About",
+                    "Features",
+                    "Pricing",
+                    "Careers",
+                    "Help",
+                    "Privacy Policy",
+                ]}
+            />
             <div className="insta"></div>
-            <div className="copyright">
+            <div className="copyright flex column">
                 © 2023 Blue Guide. All rights reserved
+                <Socials />
             </div>
-            <Socials />
         </footer>
     );
 }
